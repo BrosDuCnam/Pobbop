f3e1044dd44255663f70d251453cd6d47dcf7c56
kams <camspzlz@gmail.com>
2022-01-15 16:34:32 +0100

    Add beautiful torque effect

--- Assets/_Sources/Scripts/AbleObject/ThrowableObject.cs
+++ Assets/_Sources/Scripts/AbleObject/ThrowableObject.cs
@@ -36,7 +36,6 @@ public class ThrowableObject : MonoBehaviour
         if (IsThrown) return;
         IsThrown = true;
         _rigidbody.AddForce(transform.position + direction * (force * 50), ForceMode.Acceleration);
-        Debug.DrawRay(transform.position, transform.position + direction * (force * 50), Color.red, 10);
     }
 
     public void Throw(Vector3 step, Transform target, float speed, AnimationCurve curve)
@@ -52,7 +51,9 @@ public class ThrowableObject : MonoBehaviour
     private IEnumerator ThrowEnumerator(Vector3 step, Transform target, float speed, AnimationCurve curve)
     {
         Vector3 origin = transform.position;
-        _rigidbody.isKinematic = true;
+        Vector3 torqueDirection = -Vector3.Cross(origin - step, Vector3.up).normalized;
+        
+        //_rigidbody.useGravity = false;
         _isThrown = true;
         
         if (DEBUG)
@@ -63,19 +64,29 @@ public class ThrowableObject : MonoBehaviour
         float time = 0;
         float distance = Utils.BezierCurveDistance(origin, step, target.position, 10);
         float i = 1 / (distance / speed);
+
+        Vector3 direction = (target.position - step);
+        Vector3 lastPos = origin;
         
         while (time < 1 && !_stopThrowPath)
         {
             Vector3 nextPos = Utils.BezierCurve(origin, step, target.position, time);
-            _rigidbody.MovePosition(nextPos);
             
+            _rigidbody.MovePosition(nextPos);
+            _rigidbody.AddTorque(torqueDirection * Mathf.Pow(10, time));
+
             new WaitForEndOfFrame();
             time += (i * (curve.Evaluate(time * 3))) * Time.deltaTime; // TODO - Hacky fix for curve
+            
+            direction = nextPos - lastPos;
+            
             yield return null;
         }
         _stopThrowPath = false;
-        _rigidbody.isKinematic = false;
+        _rigidbody.useGravity = true;
         _isThrown = false;
+        
+        _rigidbody.AddForce(direction*50, ForceMode.Acceleration);
     }
 
     private void OnTriggerEnter(Collider other)
