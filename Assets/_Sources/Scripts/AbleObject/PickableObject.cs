0ad84635f812f46d77cb7c492ef2eacdb358bd61
kams <camspzlz@gmail.com>
2022-04-03 13:23:58 +0200

    Try better sync for is picked state

--- Assets/_Sources/Scripts/AbleObject/PickableObject.cs
+++ Assets/_Sources/Scripts/AbleObject/PickableObject.cs
@@ -26,6 +26,7 @@ public class PickableObject : NetworkBehaviour
         set
         {
             if (_isPicked == value) return;
+            CmdChangeIsPicked(value);
             if (value) PickUp();
             else Drop();
             _isPicked = value;
@@ -72,4 +73,14 @@ public class PickableObject : NetworkBehaviour
         if (_rigidbody != null) _rigidbody.isKinematic = false;
     }
     
+    [Command (requiresAuthority = false)]
+    private void CmdChangeIsPicked(bool value)
+    {
+        RpcChangeIsPicked(value);
+    }
+    [ClientRpc]
+    private void RpcChangeIsPicked(bool value)
+    {
+        IsPicked = value;
+    }
 }
