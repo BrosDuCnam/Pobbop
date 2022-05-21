1609a14902c0af9e5233be929e3158238bf253b2
kams <camspzlz@gmail.com>
2022-05-21 10:30:17 +0200

    Host UI test

--- Assets/_Sources/Scripts/UI/UIObjectElement.cs
+++ Assets/_Sources/Scripts/UI/UIObjectElement.cs
@@ -9,6 +9,7 @@ public class UIObjectElement : UIObject {
 	Image image;
 	GameObject message;
 	
+	
 	//public enum OutlineStyle {solidThin, solidThick, dottedThin, dottedThick};
 	public bool hasImage = false;
 	public bool isText = false;
