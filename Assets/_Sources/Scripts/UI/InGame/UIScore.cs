74bb086a2b3a0a1a3b541373bcc4cb14258d554e
kams <camspzlz@gmail.com>
2022-06-05 14:56:18 +0200

    Warning timer

--- Assets/_Sources/Scripts/UI/InGame/UIScore.cs
+++ Assets/_Sources/Scripts/UI/InGame/UIScore.cs
@@ -5,6 +5,13 @@ using UnityEngine.UI;
 
 public class UIScore : MonoBehaviour
 {
+    [SerializeField] private Color _warningColor;
+    private Color _defaultColor;
+    private bool _isWarning;
+    
+    [SerializeField] private AnimationCurve _warningCurve;
+    private float _warningTimer;
+    private float _warningTimerLimit;
     [SerializeField] private Image _fillImage;
     
     [SerializeField] private List<UIScoreTeam> teams = new List<UIScoreTeam>();
@@ -17,11 +24,11 @@ public class UIScore : MonoBehaviour
         roomProperties = RoomProperties.instance;
         gameManager = GameManager.instance;
         
+        _defaultColor = _fillImage.color;
         
         // Disable the score UI if there are no teams
         for (int i = 0; i < teams.Count; i++)
         {
-            print(teams[i].name);
             if (i >= gameManager.teams.Count)
             {
                 teams[i].gameObject.SetActive(false);
@@ -43,6 +50,24 @@ public class UIScore : MonoBehaviour
         if (roomProperties.gameLimitMode != RoomProperties.GameLimitModes.Score)
         {
             _fillImage.fillAmount = 1 - gameManager.timer / roomProperties.timerLimit;
+            
+            _warningTimer += Time.deltaTime;
+            if (_warningTimer > _warningTimerLimit)
+            {
+                _warningTimer = 0;
+                _warningTimerLimit = _warningCurve.Evaluate(gameManager.timer)/2;
+                _isWarning = !_isWarning;
+                _fillImage.color = Color.Lerp(Color.red, Color.white, _warningCurve.Evaluate(_warningTimer / _warningTimerLimit));
+            }
+
+            if (_isWarning)
+            {
+                _fillImage.color = _warningColor;
+            }
+            else
+            {
+                _fillImage.color = _defaultColor;
+            }
         }
         else
         {
