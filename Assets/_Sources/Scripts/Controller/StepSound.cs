4e645bb68d5d2e66a74bb34d9db45e4cc435e951
kams <camspzlz@gmail.com>
2022-06-20 18:31:30 +0200

    Add step sound

---
+++ Assets/_Sources/Scripts/Controller/StepSound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class StepSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] stepSounds;
+    [SerializeField] private AudioSource audioSource;
+    
+    // On Step animation event
+    public void Step()
+    {
+        print("Step");
+        
+        audioSource.clip = stepSounds[Random.Range(0, stepSounds.Length)];
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+}
